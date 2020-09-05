c04e0b61b65ddab50772674f7e70879d0ddad91f
tullio.pizz@gmail.com <151295Pt>
2020-09-05 10:59:52 +0200

    reset

--- bashScript/extract_delete_bz2.sh
+++
@@ -1,8 +0,0 @@
-base=e_04
-fileList=($(find ${base} -type f -name *.json.bz2))
-for ((i=0; i < ${#fileList[@]}; i+=1))
-do
-	bzip2 -d ${fileList[i]}
-	echo "File ${fileList[i]} estratto"	
-done
-echo "Finish"
