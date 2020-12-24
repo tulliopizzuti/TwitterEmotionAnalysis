23f7d9522c2c3355526cc9abbbe5e504a7592bc3
tullio.pizz@gmail.com <151295Pt>
2020-12-24 10:33:58 +0100

    aggiunto spark4mc

---
+++ Spark4MC/src/main/java/it/unisa/degpizz/spark/MapCounter.java
@@ -0,0 +1,47 @@
+package it.unisa.degpizz.spark;
+
+import org.chaiware.app.TextToEmotion;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MapCounter implements Serializable {
+    private Map<String, Integer> map;
+
+    public MapCounter() {
+        map = new HashMap<>();
+    }
+
+    public synchronized void parseText(String text) throws Exception{
+        increment(TextToEmotion.textToEmotionString(text));
+    }
+
+    public void increment(String key) {
+        if (map.containsKey(key)) {
+            Integer i = map.get(key);
+            i++;
+            map.put(key, i);
+        } else {
+            map.put(key, 1);
+
+        }
+    }
+
+    public Map<String, Integer> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Integer> map) {
+        this.map = map;
+    }
+
+    @Override
+    public String toString() {
+        return "MapCounter:\n" + map.entrySet().stream().map(
+                o -> String.format("%s : %d", o.getKey(), o.getValue())).
+                collect(Collectors.joining("\n"));
+
+    }
+}
